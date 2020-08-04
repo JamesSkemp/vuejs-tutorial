0b82d894d4cac758358c6d648ef291df72a38998
James Skemp <strivinglife@gmail.com>
2020-08-03 20:45:08 -0500

    Add additional WorldUtilities tests

--- __tests__/utilities/WorldUtilities.tests.ts
+++ __tests__/utilities/WorldUtilities.tests.ts
@@ -1,5 +1,8 @@
 import World from "@/models/World";
-import { startNextMoment, getUnusedPartyId } from '@/utilities/WorldUtilities';
+import { startNextMoment, getUnusedPartyId, addPartyToWorld, fixPartyIds, removeEmptyParties } from '@/utilities/WorldUtilities';
+import Party from '@/models/Party';
+import { addMainCharacter } from '@/utilities/PartyUtilities';
+import Character from '@/models/Character';
 
 const world = new World();
 
@@ -16,5 +19,38 @@ test('Starting next moment increments moment by 1', () => {
 describe('Verify party ids in world', () => {
 	test('Starting unused party id', () => {
 		expect(getUnusedPartyId(world)).toBe(0);
+	});
+
+	test('Add party with id 1', () => {
+		const party1 = new Party(1);
+		expect(addPartyToWorld(world, party1)).toBe(1);
+	});
+
+	test('Add party with id 3', () => {
+		const party3 = new Party(3);
+		expect(addPartyToWorld(world, party3)).toBe(3);
+	});
+
+	test('Fix party ids', () => {
+		fixPartyIds(world);
+		expect(world.parties.length == 2 && world.parties[0].id == 0 && world.parties[1].id == 1).toBe(true);
+	});
+
+	test('Remove empty parties', () => {
+		const party = new Party(3);
+		const character = new Character();
+		addMainCharacter(party, character);
+		addPartyToWorld(world, party);
+		removeEmptyParties(world);
+		expect(world.parties.length).toBe(1);
+	});
+
+	test('Negative party id should be positive', () => {
+		const party = new Party(-4);
+		expect(addPartyToWorld(world, party)).toBeGreaterThanOrEqual(0);
+	});
+
+	test('Passing no party creates one', () => {
+		expect(addPartyToWorld(world)).toBeGreaterThanOrEqual(0);
 	});
 });
